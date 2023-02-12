3f94b872c401495af9697847ccc63a009b23344b
Anton Belski <anton.belski@gmail.com>
2023-02-12 17:25:37 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (arg) {
+   let arr = String(Math.abs(arg)).split("");
+
+  let arr2 = arr.reverse();
+
+  let arr3 = arr2.join("");
+
+  let num = +arr3;
+  return num;
 }
